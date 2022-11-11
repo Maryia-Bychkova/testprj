5c1b9543869c5ca6e22ecfff9c39b8f6136fb030
Maryia Bychkova <mariyabchkv@gmail.com>
2022-11-12 00:36:07 +0300

    Home.Task12.11

---
+++ CSharpLinq/Horse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLinq
+{
+    internal class Horse
+    {
+        public string Name { get; set; }
+
+
+        public string Color { get; set; }
+    }
+}
