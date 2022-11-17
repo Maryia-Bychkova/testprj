61b8846ab11f104dc3db5d00dd18a7f865b7e7fa
Maryia Bychkova <mariyabchkv@gmail.com>
2022-11-18 00:49:16 +0300

    First version

---
+++ CSharpException/InvalidSideLenght.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpException
+{
+    internal class InvalidSideLenght : Exception 
+    {
+       public int Value { get; }
+
+        public InvalidSideLenght() { }
+
+        public InvalidSideLenght(string message) : base(message) { }
+
+        public InvalidSideLenght(string message, Exception inner) : base(message, inner) { }
+
+        public InvalidSideLenght(string message, int value) : base(message)
+        {
+            Value = value;
+        }
+
+        public override string? HelpLink
+        {
+            get
+            {
+                return "For more information use:www.info.com";
+            }
+        }
+    
+    }
+}
